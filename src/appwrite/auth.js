42137492bc30751445ca2d22beb136d21f32be15
Umar Khursheed <umarkhursheed979@gmail.com>
2024-12-28 04:30:20 +0530

    update

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -29,7 +29,7 @@ export class Authentication {
         try {
             return await this.account.createEmailPasswordSession(email,password)
         } catch (error) {
-            console.log("Login Error!")
+            return error
         }
     }
     async getCurrentUser(){
